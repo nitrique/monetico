1e80f40500068b48fb026ea1c8840e051bb7c554
Nicolas Guillard <nicolas@nitrique.fr>
2017-11-14 17:21:27 +0100

    Initial commit

---
+++ defines.php
@@ -0,0 +1,8 @@
+<?php
+define ('MONETICOPAIEMENT_CTLHMAC','V4.0.sha1.php--[CtlHmac%s%s]-%s');
+define ('MONETICOPAIEMENT_CTLHMACSTR', 'CtlHmac%s%s');
+define ('MONETICOPAIEMENT_PHASE2BACK_RECEIPT','version=2\ncdr=%s');
+define ('MONETICOPAIEMENT_PHASE2BACK_MACOK','0');
+define ('MONETICOPAIEMENT_PHASE2BACK_MACNOTOK','1\n');
+define ('MONETICOPAIEMENT_PHASE2BACK_FIELDS', '%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*');
+define ('MONETICOPAIEMENT_PHASE1GO_FIELDS', '%s*%s*%s%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s');
